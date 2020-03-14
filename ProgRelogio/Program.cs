e8a6ff7e402b809873a482f03886edaa732095ec
Marlon Hugo Rodrigues da Silva <marlon.hugo@unis.edu.br>
2020-03-13 21:48:49 -0300

    Inicio do projeto

---
+++ ProgRelogio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgRelogio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double entrada = System.Convert.ToDouble(Console.ReadLine());
+            Relogio R = new Relogio(entrada);
+
+            double xx = R.Hora;
+            System.Console.WriteLine(xx);
+
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ ProgRelogio/Rolegio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgRelogio
+{
+    class Relogio
+    {
+        private double segundos;
+
+        /*Construtor*/
+        public Relogio(double seg)
+        {
+            segundos = seg;
+        }
+
+        public double Segundos
+        {
+            get { return segundos; }
+            set
+            {
+                if (value == 0)
+                    System.Console.WriteLine("Segundos não pode ser zero.");
+                segundos = value;
+            }
+        }
+
+        public double Minuto
+        {
+            get;
+            set;
+        }
+
+        public double Hora
+        {
+            get { return segundos / 3600; }
+            set
+            {
+                if (value <= 1 && value >= 24)
+                    System.Console.WriteLine("Valor deve ser entre 1 e 24");
+                segundos = value * 3600;
+            }
+        }
+    }
+}
+
+
+
